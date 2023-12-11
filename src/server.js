ff8ecf42f753447f716d513c5d2f1b5bf59903f4
minsoftk <minsoftk@gmail.com>
2023-12-11 13:44:16 +0900

    conf: first commit

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express, { application } from 'express';
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+app.use('/public', express.static(__dirname + '/public'));
+app.get('/', (req, res) => res.render('home'));
+
+const handleListen = () => {
+  console.log('localhost:3000');
+};
+app.listen(3000, handleListen);
