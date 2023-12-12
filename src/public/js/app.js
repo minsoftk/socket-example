12fd0b4593d1d3815a1840b9ec4bc000deff3659
minsoftk <minsoftk@gmail.com>
2023-12-12 16:46:55 +0900

    feat: WebSocket example

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,44 @@
-hello;
+// socket은 서버로의 연결을 의미한다.
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('#message');
+const nickForm = document.querySelector('#nick');
+
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  return JSON.stringify({ type, payload });
+}
+
+socket.addEventListener('open', (socket) => {
+  console.log('connect Server');
+});
+
+socket.addEventListener('message', (message) => {
+  // console.log('just got this:', message, 'from the server');
+
+  const li = document.createElement('li');
+  li.innerText = message.data.toString('utf-8');
+  messageList.append(li);
+});
+
+socket.addEventListener('close', (message) => {
+  console.log('disconnected from Server');
+});
+
+messageForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const input = messageForm.querySelector('input');
+  socket.send(makeMessage('new_message', input.value));
+  input.value = '';
+});
+
+nickForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+  input.value = '';
+});
+
+// setTimeout(() => {
+//   socket.send('hello from the browser');
+// }, 10000);
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
-import express, { application } from 'express';
-
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 const app = express();
 
 app.set('view engine', 'pug');
@@ -8,6 +9,35 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render('home'));
 
 const handleListen = () => {
-  console.log('localhost:3000');
+  console.log('ws & http://localhost:3000');
 };
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+// socket은 연결된 브라우저를 의미한다
+function handleConnection(socket) {
+  console.log(socket);
+}
+
+const sockets = [];
+wss.on('connection', (socket) => {
+  console.log('connect Browser');
+  sockets.push(socket);
+  socket['nickname'] = 'Anonmyous';
+  socket.on('close', () => console.log('disconnect from browswer!'));
+  socket.on('message', (message) => {
+    const { type, payload } = JSON.parse(message);
+    console.log('type, payload :', type, payload);
+    if (type === 'new_message') {
+      sockets.forEach((asocket) => {
+        asocket.send(`${socket.nickname}: ${payload}`);
+      });
+    } else if (type === 'nickname') {
+      socket['nickname'] = payload;
+    }
+  });
+});
+server.listen(3000, handleListen);
